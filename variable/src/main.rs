d88ef2d674ad6caf87db4c3cc705e74ec561bd18
bankaryogesh <bankaryogesh20@gmail.com>
2025-11-04 18:08:52 +0530

    Initial commit with variable program

---
+++ variable/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let age: i32 = 25; // integer variable
+    let name: String = String::from("Alice"); // string variable
+    
+
+    println!("Name: {}, Age: {}", name, age);
+}
+
+
+    
