6a6dfa99d7670b77fdacc300fdbfa6a793ae8c2a
bankaryogesh <bankaryogesh20@gmail.com>
2025-11-05 21:40:59 +0530

    variable update

--- variable/src/main.rs
+++ variable/src/main.rs
@@ -1,9 +1,13 @@
 fn main() {
     let age: i32 = 25; // integer variable
     let name: String = String::from("Alice"); // string variable
+
+    // let name1_gender: char = 'f'; //char variable
+
+    let name_gender = String::from("female");
     
 
-    println!("Name: {}, Age: {}", name, age);
+    println!("Name: {}, Age: {}, name_gender:  {}", name, age, name_gender);
 }
 
 
