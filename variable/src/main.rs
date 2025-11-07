5ecf8b8196ccdda54d4abc54aa7e0a8b0952eb00
bankaryogesh <bankaryogesh20@gmail.com>
2025-11-07 12:27:51 +0530

    1

--- variable/src/main.rs
+++ variable/src/main.rs
@@ -5,9 +5,11 @@ fn main() {
     // let name1_gender: char = 'f'; //char variable
 
     let name_gender = String::from("female");
+
+    let vars1: String = String::from("male"); // This will cause a compile-time error due to invalid variable name
     
 
-    println!("Name: {}, Age: {}, name_gender:  {}", name, age, name_gender);
+    println!("Name: {}, Age: {}, name_gender: {}, vars1: {}", name, age, name_gender, vars1);
 }
 
 
